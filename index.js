a775a58a59c4d371c032baf93f4c1ebae8426e41
Deepak Barwal <deepak.barwal@redsoftware.in>
2024-02-14 19:17:16 +0530

    feat: initial working version

--- index.js
+++ index.js
@@ -7,3 +7,39 @@
  *
  * @author Deepak <https://www.linkedin.com/in/d-barwal/>
  */
+import chalk from 'chalk';
+import sym from 'log-symbols';
+
+const green = chalk.green;
+const greenInverse = chalk.green.bold.inverse;
+const red = chalk.red.bold;
+const redInverse = chalk.red.bold.inverse;
+const orange = chalk.hex('#FFA500');
+const orangeInverse = chalk.hex('#FFA500').bold.inverse;
+const blue = chalk.blue;
+const blueInverse = chalk.blue.bold.inverse;
+
+export default options => {
+	const defaultOptions = {
+		type: `error`,
+		msg: `You forgot to define all options.`
+	};
+	const opts = {...defaultOptions, ...options};
+	const {type, msg} = opts;
+
+	if (type === `success`) {
+		console.log(`\n${sym.success}  ${greenInverse(` SUCCESS `)} ${green(msg)}\n`);
+	}
+
+	if (type === `error`) {
+		console.log(`\n${sym.error}  ${redInverse(` ERROR `)} ${red(msg)}\n`);
+	}
+
+	if (type === `warning`) {
+		console.log(`\n${sym.warning}  ${orangeInverse(` WARNING `)} ${orange(msg)}\n`);
+	}
+
+	if (type === `info`) {
+		console.log(`\n${sym.success}  ${blueInverse(` INFO `)} ${blue(msg)}\n`);
+	}
+};
--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-const alert = require('./index.js');
+import alert from './index.js';
+
+alert();
 
 alert({
-	type: 'success',
-	msg: 'All done!'
+	type: `success`,
+	msg: `All done!`
+});
+
+alert({
+	type: `warning`,
+	msg: `You didn't add something!`
+});
+
+alert({
+	type: `info`,
+	msg: `You're awesome!`
+});
+
+alert({
+	type: `error`,
+	msg: `Something went wrong!`
 });
