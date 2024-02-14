bfa0002cfaa0f8633a50e563eff9466a33512981
Deepak Barwal <deepak.barwal@redsoftware.in>
2024-02-14 18:16:33 +0530

    feat: initialized new npm project

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * CLI Alerts
+ *
+ * Cross platform CLI Alerts with colors.
+ * Works on macOS, Linux and Windows.
+ * Alerts: `success`, `info`, `warning`, `error`.
+ *
+ * @author Deepak <https://www.linkedin.com/in/d-barwal/>
+ */
